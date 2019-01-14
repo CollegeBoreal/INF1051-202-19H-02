4f0c09ffb5817f0f9aabcb2e44378a6829f259da
safaa <safaazaoui@gmail.com>
2019-01-14 16:33:07 -0500

    safaa

--- 0.RxJS/b300108495/src/app/timer/timer.component.ts
+++ 0.RxJS/b300108495/src/app/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {EMPTY, empty, fromEvent, interval, merge, timer} from 'rxjs';
+import {EMPTY, empty, fromEvent, interval, merge, Observable, Subscription, timer} from 'rxjs';
 import {mapTo, scan, startWith, switchMap, takeWhile} from 'rxjs/operators';
 
 @Component({
@@ -12,7 +12,24 @@ export class TimerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const source = timer(1000, 2000);
-    const subscribe = source.subscribe(val => console.log(val));
+    const secondsCounter = interval(1000);
+    secondsCounter.subscribe(n =>
+      console.log(`It's been ${n} seconds since subscribing!`));
+    const el = document.getElementById('my-element');
+
+// Create an Observable that will publish mouse movements
+    const mouseMoves: Observable<Event> = fromEvent(el, 'mousemove');
+
+// Subscribe to start listening for mouse-move events
+    const subscription: Subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+      // Log coords of mouse movements
+      console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
+
+      // When the mouse is over the upper-left of the screen,
+      // unsubscribe to stop listening for mouse movements
+      if (evt.clientX < 40 && evt.clientY < 40) {
+        subscription.unsubscribe();
+      }
+    });
   }
 }
