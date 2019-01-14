43ce9dc6ee992570e3a8357bebd3b9bb23d4b724
Brice Robert <300098957@collegeboreal.ca>
2019-01-14 16:36:44 -0500

    :music:

--- 0.RxJS/b300098957/src/app/timer/timer.component.ts
+++ 0.RxJS/b300098957/src/app/timer/timer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {mapTo, scan, startWith, switchMap, takeWhile} from 'rxjs/operators';
-import {EMPTY, interval, merge, Observable} from 'rxjs';
+import {EMPTY, fromEvent, interval, merge, Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -11,18 +11,57 @@ export class TimerComponent implements OnInit {
 
   // variable d'instance
   html = '';
+  /*
+    countdownSeconds = 10;
+    pause$: Observable<boolean>;
+    resume$: Observable<boolean>;
+    interval$ = interval(1000).pipe(mapTo(-1));
+    timer$ = merge(this.pause$, this.resume$)
+      .pipe(
+        startWith(true),
+        // if timer is paused return empty observable
+        switchMap(val => (val ? this.interval$ : EMPTY)),
+        scan((acc, curr) => (curr ? curr + acc : acc), this.countdownSeconds),
+        takeWhile(v => v >= 0)
+      )
+      .subscribe({next: value => this.html = '' + value });
+  */
 
   constructor() { }
 
   ngOnInit() {
+    // Create an Observable that will publish a value on an interval
+    const secondsCounter: Observable<number> = interval(1000);
+// Subscribe to begin publishing values
+    secondsCounter.subscribe(n =>
+      console.log(`It's been ${n} seconds since subscribing!`));
+
+    const el = document.getElementById('my-element');
+
+// Create an Observable that will publish mouse movements
+    const mouseMoves: Observable<Event> = fromEvent(el, 'mousemove');
+
+// Subscribe to start listening for mouse-move events
+    const subscription: Subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+      // Log coords of mouse movements
+      console.log(`Coords: ${evt.clientX} X ${evt.clientY}`);
+
+      // When the mouse is over the upper-left of the screen,
+      // unsubscribe to stop listening for mouse movements
+      if (evt.clientX < 40 && evt.clientY < 100) {
+        subscription.unsubscribe();
+      }
+    });
   }
 
   pauseButton(event) {
+    // this.pause$ = EMPTY.pipe(mapTo(false));
     this.html = 'coucou';
     console.log('coucou');
   }
 
   resumeButton(event) {
+    // this.resume$ = EMPTY.pipe(mapTo(true));
     this.html = 'ici';
     console.log('ici');
   }
