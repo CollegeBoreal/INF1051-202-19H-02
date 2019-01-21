c1f96ffb7805289f30ddfecceef1727462be9947
ameliedubois <fichou@hotmail.com>
2019-01-21 15:33:37 -0500

    Amelie

---
+++ 1.Service/b300089781/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {PokemonService} from './pokemon.service';
+import {map, mergeMap} from 'rxjs/operators';
+import {ConfigService} from './config.service';
+import {Config} from './config';
+import {PokemonResult} from './pokemon-result';
+import {Pokemon} from './pokemon';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'b300089781';
+  config: Config;
+  pokemons: PokemonResult;
+
+  constructor(
+    private pokemonService: PokemonService,
+    private configService: ConfigService
+  ) {}
+
+  ngOnInit(): void {
+    this.configService.getConfig()
+      .subscribe((data: Config) => this.config = {
+        heroesUrl: data.heroesUrl,
+        textfile: data.textfile
+      });
+
+    this.pokemonService.pokemonInit()
+      .subscribe((data: PokemonResult) => this.pokemons = data);
+  }
+}
