7cf0cf68f85c53dd2d97a74c6a624eb5772450ac
safaa <safaazaoui@gmail.com>
2019-03-19 21:46:38 -0400

    safaa

---
+++ 3.Nebular/bb300108495/src/app/pages/pages-colors/pages-colors.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-colors',
+  template: `
+    <p>
+      pages-colors works!
+    </p>
+  `,
+  styles: []
+})
+export class PagesColorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
