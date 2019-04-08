08c0ecd243cdd57b7685e5780eb3ad150817e691
ameliedubois <fichou@hotmail.com>
2019-04-08 14:17:53 -0400

    Amelie

--- 4.Forms/b300089781/src/app/artists/artists.component.ts
+++ 4.Forms/b300089781/src/app/artists/artists.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {ArtistsListService} from '../artists-list.service';
 
 @Component({
   selector: 'app-artists',
   templateUrl: './artists.component.html',
   styleUrls: ['./artists.component.scss']
 })
-export class ArtistsComponent {
+export class ArtistsComponent implements OnInit {
 
+  Artists = [];
   name = new FormControl('');
   updateName() {
     this.name.setValue('Moi');
+    this.artistsListService.setData(this.name.value);
   }
-
-  Artists = [];
-
   constructor(private artistsListService: ArtistsListService) {
   }
 
---
+++ 4.Forms/b300089781/src/app/customer/customer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {CustomerListService} from '../customer-list.service';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent implements OnInit {
+
+  Customer = [];
+  name = new FormControl('');
+  updateName() {
+    this.name.setValue('Moi');
+    this.customerListService.setData(this.name.value);
+  }
+  constructor(private customerListService: CustomerListService) {
+  }
+
+  ngOnInit() {
+    console.log('populate data');
+    this.customerListService.getData()
+      .subscribe(
+        (data: any) => this.Customer = data
+        , (err: any) => console.error('too bad' + err)
+      );
+  }
+
+}
